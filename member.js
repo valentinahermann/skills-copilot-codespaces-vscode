9fbbff3d6e7b6ad81b9b23484a5b32b94b19b27d
Valentina Hermann <72870031+valentinahermann@users.noreply.github.com>
2023-11-15 17:14:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,5 @@
+function skillsMember() {
+  var skills = document.getElementById("skills");
+  var skillsMember = document.getElementById("skillsMember");
+  var skillsMemberValue = skillsMember
+}
